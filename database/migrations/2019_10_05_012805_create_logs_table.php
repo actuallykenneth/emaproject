cb8221d54f1e98c4e9359af884ba0bee58753b3b
Aqureshi06 <qureshia211217@gmail.com>
2019-10-04 20:37:42 -0500

    emaproject(V.001) - Added database

---
+++ database/migrations/2019_10_05_012805_create_logs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('role');
+            $table->string('activity');
+            $table->string('time_in');
+            $table->string('time_out');
+            $table->string('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
