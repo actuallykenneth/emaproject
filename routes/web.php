b666f496944c222d57085f783c908c967bcdbbca
cultleader <oddcultleader@gmail.com>
2019-10-06 03:02:24 -0500

    Basic authentication middleware added.

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/testlogin', function () {
+    return view('testlogin');
+})->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
